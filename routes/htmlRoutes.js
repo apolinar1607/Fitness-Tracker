e5183a6cf080740a6ebf89534d27a2cc983f439a
Junarsan <apolinar.magtanong@gmail.com>
2021-04-11 21:39:56 +1000

    Modified rendering expression in htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,17 @@
 const path = require("path");
 
 module.exports = function(app) {
-  app.get("*", function(req, res) {
+  //to render Home page
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  //to render Exercise page
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  })
+  //to render Stats page
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  })
 };
