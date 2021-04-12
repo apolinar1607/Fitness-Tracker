53dd8bb626ea329525fdb0b0f88914dcc00d3b17
Junarsan <apolinar.magtanong@gmail.com>
2021-04-12 20:56:22 +1000

    Modify apiRoutes.js to include post and put methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,6 +12,37 @@ module.exports = function (app) {
                 res.json(message);
             })
     });
+    //retrieve all workouts in range
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find()
+            .then(data => {
+                console.log(data);
+                res.json(data)
+            })
+            .catch(({ message }) => {
+                res.json(message);
+            });
+    });
+    //only applicable if location search is undefined
+    app.post("/api/workouts", ({body}, res) => {
+        db.Workout.create(body)
+            .then(data => {
+                res.json(data);
+            })
+            .catch(({ message }) => {
+                res.json(message);
+            });
+    });
 
+    //PUT/Update workout
+    app.put("/api/workouts/:id", ({body, params}, res) => {
+        db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}})
+            .then(data => {
+                res.json(data);
+            })
+            .catch(({message}) => {
+                res.json(message);
+            });
+    });
 
 }
